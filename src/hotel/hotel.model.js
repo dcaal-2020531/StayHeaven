1c3866b1a0c2f28d99aaa826bdc4e12d5aac7ad2
dcaal-2020531 <dcaal-2020531@kinal.edu.gt>
2025-04-06 19:01:16 -0600

    Se agrega la carpeta y el modelo de Hotel

---
+++ src/hotel/hotel.model.js
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose'
+
+const hotelSchema = Schema(
+    { 
+        name:{
+            type: String,
+            required: [true, 'Name is required']
+        },
+        addres:{
+            type: String,
+            required: [true, 'Adress is required']
+        },
+        phone:{
+            type: String,
+            required: [true, 'Phone is required']
+        },
+        email:{
+            type: String,
+            required:[true, 'Email is required'],
+            unique: true
+        },
+        country:{
+            type:String,
+            required:[true,'Country is required']
+        },
+        city:{
+            type:String,
+            required:[true,'City is required']
+        }
+    }
+)
+
+
+export default model('Hotel', hotelSchema)
